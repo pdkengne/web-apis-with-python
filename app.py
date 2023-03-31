c2ae335cbccdac581d056da701aa64d32e8b2a8d
pdkengne <sielenou_alain@yahoo.fr>
2023-03-31 21:36:18 +0200

    adding app.py file

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template, redirect
+
+app = Flask(__name__)
+
+@app.get("/")
+@app.get("/")
+def index ():
+    return render_template( "index.html" )
+
+@app.get("/search")
+def search ():
+    args = request.args.get( "q" )
+    return redirect( f"https://google.com/search?q= {args} " )
+
+
+if __name__ == "__main__":
+    app.run()
