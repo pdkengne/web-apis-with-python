1553d867424187ae0f71817de0717b8123d6d09c
pdkengne <sielenou_alain@yahoo.fr>
2023-04-01 17:38:39 +0200

    adding the app.py file

---
+++ app.py
@@ -0,0 +1,49 @@
+# dictionary-api-python-flask/app.py
+from flask import Flask, request, jsonify, render_template
+from model.dbHandler import match_exact, match_like
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def index():
+    """
+    DEFAULT ROUTE
+    This method will
+    1. Provide usage instructions formatted as JSON
+    """
+    response = {"usage": "/dict?=<word>"}
+    # Since this is a website with front-end, we don't need to send the usage instructions
+    return jsonify(response)
+
+
+@app.get("/dict")
+def dictionary():
+    """
+    DEFAULT ROUTE
+    This method will
+    1. Accept a word from the request
+    2. Try to find an exact match, and return it if found
+    3. If not found, find all approximate matches and return
+    """
+
+    word = request.args.get( "word" )
+    # Return an error querystring is malformed
+    if not word:
+        return jsonify({ "status" : "error" , "data" : "word not found" })
+    # Try to find an exact match
+    definitions = match_exact(word)
+    if definitions:
+        return jsonify({ "status" : "success" , "data" : definitions})
+    
+    # Try to find an approximate match
+    definitions = match_like(word)
+    if definitions:
+        return jsonify({ "status" : "partial" , "data" : definitions})
+    else :
+        return jsonify({ "status" : "error" , "data" : "word not found" })
+
+
+
+if __name__ == "__main__":
+    app.run()
